557a46f4228a26fe0bd311c7c6f12a52e78b6bc4
mohammadnoorbaba <148877596+mohammadnoorbaba@users.noreply.github.com>
2026-03-01 20:54:37 +0530

    Update qualitychecks.sql

--- scripts/silver/qualitychecks.sql
+++ scripts/silver/qualitychecks.sql
@@ -1,3 +1,4 @@
+-- Table 1 bronze.crm_cust_info
 select
 *
 from bronze.crm_cust_info
@@ -41,4 +42,54 @@ from bronze.crm_cust_info
 where cst_gndr != TRIM(cst_gndr)
 
 -- we observe from this we have unwanted spaces from both columns firstname and lastname
+
+
+-- Table 2 : bronze.crm_prd_info
+
+
+
+-- checking duplicates in primary key or any null value in primary key
+select
+prd_id,
+count(*)
+from bronze.crm_prd_info
+group by prd_id
+having count(*) > 1 OR prd_id IS NULL
+
+-- checking for any unwanted spaces and nulls
+select
+prd_key
+from bronze.crm_prd_info
+where prd_key != trim(prd_key) OR prd_key IS NULL
+
+
+select
+prd_nm
+from bronze.crm_prd_info
+where prd_nm != trim(prd_nm) OR prd_nm IS NULL
+
+
+-- data quality if cost any product is negative or null
+select
+prd_cost
+from bronze.crm_prd_info
+where prd_cost < 0 OR prd_cost IS NULL
+
+-- checking for cardinality
+select distinct
+prd_line
+from bronze.crm_prd_info
+-- counting nulls
+select
+prd_end_dt,
+count(*)
+from bronze.crm_prd_info
+where prd_end_dt IS NULL
+group by prd_end_dt
+having count(*) > 1
+
+
+
+
+
 
